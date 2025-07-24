37367982a2b1d6e2f256e3e119fd4230ba388920
Martin Foll <martin.foll@cern.ch.>
2025-07-24 11:07:11 +0200

    remove TTree source requirement

--- bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_batchgenerator.py
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_batchgenerator.py
@@ -162,10 +162,10 @@ class BaseGenerator:
 
         self.noded_rdf = RDF.AsRNode(rdataframe)
 
-        if ROOT.Internal.RDF.GetDataSourceLabel(self.noded_rdf) != "TTreeDS":
-            raise ValueError(
-                "RNode object must be created out of TTrees or files of TTree"
-            )
+        # if ROOT.Internal.RDF.GetDataSourceLabel(self.noded_rdf) != "TTreeDS":
+        #     raise ValueError(
+        #         "RNode object must be created out of TTrees or files of TTree"
+        #     )
 
         if isinstance(target, str):
             target = [target]
